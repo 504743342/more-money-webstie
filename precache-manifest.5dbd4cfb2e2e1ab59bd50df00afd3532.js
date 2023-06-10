9054c2843f001f2f688d66b2c181eaebae00800e
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-06-10 12:11:23 +0800

    deploy

---
+++ precache-manifest.5dbd4cfb2e2e1ab59bd50df00afd3532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d796f4917308dcac68d0",
+    "url": "/more-money-webstie/css/app.e7fba9b2.css"
+  },
+  {
+    "revision": "dc0c7067b3b4e2eddb4b96310fed6fd9",
+    "url": "/more-money-webstie/index.html"
+  },
+  {
+    "revision": "d796f4917308dcac68d0",
+    "url": "/more-money-webstie/js/app.b8d64b45.js"
+  },
+  {
+    "revision": "a25d2d999387e47ca853",
+    "url": "/more-money-webstie/js/chunk-vendors.f66220c8.js"
+  },
+  {
+    "revision": "42bfa4ba9edfb3426e53b3524294dc7f",
+    "url": "/more-money-webstie/manifest.json"
+  },
+  {
+    "revision": "9984f5aca90926c30bc4d0c4143908fd",
+    "url": "/more-money-webstie/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/more-money-webstie/robots.txt"
+  }
+]);
